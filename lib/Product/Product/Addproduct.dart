af2c2976fcdc258c06e9b36903cd8bc81e03d66b
Tus <zombieman456@hotmail.com>
2021-03-06 22:20:49 +0700

    BasketScreen

---
+++ lib/Product/Product/Addproduct.dart
@@ -0,0 +1,231 @@
+import 'dart:io';
+import 'package:Shopper/Homepage/BottomNavigation.dart';
+import 'package:Shopper/api/product_api.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class Addproduct extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Addproduct();
+  }
+}
+
+class _Addproduct extends State<Addproduct> {
+  String dropdownValue = 'Nike';
+  final picker = ImagePicker();
+  File _image;
+  productApi api = productApi();
+
+  @override
+  Widget build(BuildContext context) {
+    // var provider = Provider.of<ProductProvider>(context, listen: false);
+    // Product product = provider.getProduct();
+    TextEditingController NameproductController = TextEditingController();
+    TextEditingController InformationController = TextEditingController();
+    TextEditingController PriceController = TextEditingController();
+    TextEditingController BrandController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Addproduct"),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 75, vertical: 1),
+            child: Container(
+              child: _image == null
+                  ? Image.asset("assets/images/air-max-90.jpg")
+                  : Image.file(File(_image.path)),
+            ),
+          ),
+          DropdownButton<String>(
+            value: dropdownValue,
+            onChanged: (String newValue) {
+              setState(() {
+                dropdownValue = newValue;
+                print(dropdownValue);
+              });
+            },
+            items: <String>['Nike', 'Converse', 'Puma', 'Adidas', 'Vans']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+
+          TextFormField(
+            controller: NameproductController,
+            decoration: new InputDecoration(labelText: "กรุณาป้อนชื่อProduct"),
+            autofocus: true,
+            validator: (String str) {
+              //ชื่อรายการเป็นค่าว่าง
+              if (str.isEmpty) {
+                return "กรุณาป้อนชื่อProduct";
+              }
+              return null;
+            },
+          ),
+          //
+          TextFormField(
+            controller: InformationController,
+            decoration:
+                new InputDecoration(labelText: "กรุณาป้อนข้อมูลProduct"),
+            autofocus: true,
+            cursorColor: Colors.redAccent,
+            validator: (String str) {
+              if (str.isEmpty) {
+                return "กรุณาป้อนข้อมูลProduct";
+              }
+
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: PriceController,
+            decoration: new InputDecoration(labelText: "กรุณาป้อนราคาProduct"),
+            autofocus: true,
+            validator: (String str) {
+              if (str.isEmpty) {
+                return "กรุณาป้อนราคาProduct";
+              }
+
+              return null;
+            },
+          ),
+
+          RaisedButton(
+            onPressed: () async {
+              var res = await api.uploadproduct(
+                _image,
+                NameproductController.text,
+                InformationController.text,
+                PriceController.text,
+                dropdownValue.toString(),
+              );
+              print(res.statusCode);
+
+              if (res.statusCode == 200) {
+                //Map<String, dynamic> data = jsonDecode(res.body);
+                print("yes");
+
+                Alert(
+                  context: context,
+                  type: AlertType.success,
+                  title: "เพิ่มสินค้าสำเร็จ",
+                  desc: "",
+                  buttons: [
+                    DialogButton(
+                        child: Text(
+                          "OK",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        onPressed: () => Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return Bottomnavigations(
+                                selectedIndex: 0,
+                              );
+                            })))
+                  ],
+                ).show();
+              }
+            },
+            color: Colors.deepOrange,
+            child: Text(
+              'ยืนยัน',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.black,
+        child: InkWell(
+          onTap: () {
+            showModalBottomSheet(
+              context: context,
+              builder: ((builder) => bottomSheet()),
+            );
+          },
+          child: Icon(
+            Icons.camera_alt,
+            color: Colors.white,
+            size: 50.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Choose Profile photo",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.camera),
+              onPressed: () {
+                takePhoto2(ImageSource.camera);
+              },
+              label: Text("Camera"),
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.image),
+              onPressed: () {
+                takePhoto(ImageSource.gallery);
+              },
+              label: Text("Gallery"),
+            ),
+          ])
+        ],
+      ),
+    );
+  }
+
+  void takePhoto(ImageSource source) async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  void takePhoto2(ImageSource source) async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+}
---
+++ lib/Product/Product/All_product.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+
+import 'package:Shopper/Product/Product/Addproduct.dart';
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'detail_page.dart';
+
+class OnitsukaTiger2 extends StatefulWidget {
+  String brand;
+  OnitsukaTiger2({this.brand});
+
+  @override
+  _OnitsukaTiger2State createState() => _OnitsukaTiger2State();
+}
+
+class _OnitsukaTiger2State extends State<OnitsukaTiger2> {
+  String brand = "";
+  Future getproduct() async {
+    try {
+      var response = await http
+          .get('http://192.168.43.200:5000/product/select?brand=' + brand);
+
+      return json.decode(response.body);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  void initState() {
+    brand = widget.brand;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(brand),
+          backgroundColor: Colors.black,
+        ),
+        body: Container(
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              if (ConnectionState.active != null && !snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              if (ConnectionState.done != null && snapshot.hasError) {
+                return Center(child: Text(snapshot.error));
+              }
+
+              return GridView.builder(
+                primary: false,
+                padding: const EdgeInsets.all(15),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 1, mainAxisSpacing: 1, crossAxisCount: 2),
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) =>
+                              DetailPage(id: snapshot.data[index]["ID"]),
+                        ),
+                      );
+                    },
+                    child: Container(
+                        margin: EdgeInsets.only(left: 1, right: 8, bottom: 10),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 1, horizontal: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              spreadRadius: 10,
+                              blurRadius: 10,
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Image.network(
+                              'http://192.168.43.200:5000/product/image?path=' +
+                                  snapshot.data[index]["Images"],
+                              width: 160,
+                              height: 110,
+                            ),
+                            Text("${snapshot.data[index]["Name"]}",
+                                maxLines: 2,
+                                overflow: TextOverflow.fade,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.monetization_on,
+                                  color: Colors.yellow,
+                                  size: 25.0,
+                                ),
+                                Text(
+                                    "        ${snapshot.data[index]["Price"]}  ฿",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.fade,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ],
+                            )
+                          ],
+                        )),
+                  );
+                },
+              );
+            },
+            future: getproduct(),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Addproduct()),
+            );
+          },
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          mini: true,
+          child: Icon(Icons.add),
+        ));
+  }
+}
